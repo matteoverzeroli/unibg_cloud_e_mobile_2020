3050a20ddf12bb14d31c9ca1609e70242fbcab66
davidesalvetti-unibg <81446858+davidesalvetti-unibg@users.noreply.github.com>
2021-06-02 18:32:29 +0200

    Update CreateDataLake.py

--- Homework_2/CreateDataLake.py
+++ Homework_2/CreateDataLake.py
@@ -29,7 +29,7 @@ tedx_dataset = spark.read \
     .option("header","true") \
     .option("quote", "\"") \
     .option("escape", "\"") \
-    .option("multiline", "true" \
+    .option("multiline", "true") \
     .csv(tedx_dataset_path)
     
 tedx_dataset.printSchema()
