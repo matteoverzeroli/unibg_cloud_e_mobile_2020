18277ff43463bcf74044ba6231d1125664dbf7ef
Matteo Verzeroli <49780669+matteoverzeroli@users.noreply.github.com>
2021-06-06 10:48:14 +0200

    Update Add_Comment_To_Talk.js
    add some comment

--- Homework_3/Add_Comment_To_Talk/Add_Comment_To_Talk.js
+++ Homework_3/Add_Comment_To_Talk/Add_Comment_To_Talk.js
@@ -7,11 +7,13 @@ const talk = require('./Talk');
 module.exports.add_comment = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
     console.log('Received event:', JSON.stringify(event, null, 2));
+    
     let body = {}
+    //parse body content
     if (event.body) {
         body = JSON.parse(event.body)
     }
-
+    //control if id is null
     if(!body.id) {
         callback(null, {
                     statusCode: 500,
@@ -19,7 +21,7 @@ module.exports.add_comment = (event, context, callback) => {
                     body: 'Could not add the comment. Id is null.'
         })
     }
-    
+    //control if comment is null
     if (!body.comment) {
         callback(null, {
                     statusCode: 500,
@@ -27,7 +29,7 @@ module.exports.add_comment = (event, context, callback) => {
                     body: 'Could not add the comment. Comment is null.'
         })
     }
-    
+    //control if user_id is null
     if (!body.user_id) {
         callback(null, {
                     statusCode: 500,
@@ -38,19 +40,21 @@ module.exports.add_comment = (event, context, callback) => {
     
     connect_to_db().then(() => {
         console.log('=> push comment');
+        
         talk.find({_id: body.id})
         .then(tedtalk => {
             
             console.log("TedTalk:" + tedtalk)
-            
+            //control if any talk is found
             if (isEmpty(tedtalk)) {
                 callback(null, {
                     statusCode: 500,
                     body: 'Could not find the talk.'
                 })
             }
-            
+            //get current date
             let date = new Date()
+            //update talk in MongoDB
             talk.update(
                 {_id: body.id},
                 { "$push": 
@@ -65,7 +69,7 @@ module.exports.add_comment = (event, context, callback) => {
             ).then(updated => {
                 callback(null, {
                     statusCode: 200,
-                    body: "UPDATED!"
+                    body: "UPDATED!" //return answer 
                 })
             })
             .catch(err =>
@@ -88,6 +92,7 @@ module.exports.add_comment = (event, context, callback) => {
     });
 };
 
+//control if an object is empty
 function isEmpty(obj) {
 
     // null and undefined are "empty"
